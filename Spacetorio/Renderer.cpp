c6d53219b3fddf8453beccb5bab4e51affb0ebe2
Stefano Bertoli <elkiwydev@gmail.com>
2023-02-01 19:35:37 +0100

    Fixed OpenGL version selection for MacOS

--- Spacetorio/Renderer.cpp
+++ Spacetorio/Renderer.cpp
@@ -53,6 +53,7 @@ Renderer::Renderer(SDL_Window* sdlWin, iSize sr) : screenRes(sr){
 
     //Predefine GL Versions and attributes
     const char* glsl_version = "#version 330";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
